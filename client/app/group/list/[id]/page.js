426d7f7193e2491c3a642e8ef8a5f24d5dfddb76
Hsinwen <wc5894@gmail.com>
2025-03-06 09:02:48 +0800

    測試

--- client/app/group/list/[id]/page.js
+++ client/app/group/list/[id]/page.js
@@ -138,7 +138,7 @@ export default function GroupDetailPage() {
       const res = await axios.post(api + "/group/join", joinInformation);
       if (res.data.status == "success") {
         alert("成功跟團");
-        window.location = `/member/group`;
+        window.location = `/admin/group`;
       } else {
         alert(res.data.message || "跟團失敗！請稍後再試");
       }
